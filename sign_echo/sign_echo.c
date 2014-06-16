7529e9b56901c0e5807f4cfaccbd8a0506534674
Matt Rockar <mrockar@gmail.com>
2014-06-16 20:44:07 +0000

    Initial commit for linux sign_echo utility, which writes input from stdin to a betabrite sign.

---
+++ sign_echo/sign_echo.c
@@ -0,0 +1,303 @@
+#include "sign_echo.h"
+
+#define BUFFER_SIZE 512
+#define NUM_PORTS 30
+#define NUM_SPEEDS 6
+#define NUM_COLORS 12
+#define NUM_POSITIONS 4
+#define NUM_MODES 19
+#define NUM_SPECIAL_MODES 10
+
+static ctrl_code_t speeds[] = {
+    {"no_hold", NO_HOLD},
+    {"speed_1", SPEED_1}, 
+    {"speed_2", SPEED_2}, 
+    {"speed_3", SPEED_3}, 
+    {"speed_4", SPEED_4}, 
+    {"speed_5", SPEED_5}
+};
+
+static ctrl_code_t colors[] = {
+    {"red",         COLOR_RED},
+    {"green",       COLOR_GREEN},
+    {"amber",       COLOR_AMBER},
+    {"dim_red",     COLOR_DIM_RED},
+    {"dim_green",   COLOR_DIM_GREEN},
+    {"brown",       COLOR_BROWN},
+    {"orange",      COLOR_ORANGE},
+    {"yellow",      COLOR_YELLOW},
+    {"rainbow_1",   COLOR_RAINBOW1},
+    {"rainbow_2",   COLOR_RAINBOW2},
+    {"color_mix",   COLOR_COLOR_MIX},
+    {"autocolor",   COLOR_AUTOCOLOR}
+};
+
+static ctrl_code_t positions[] = {
+    {"middle",  POS_MIDDLE_LINE},
+    {"top",     POS_TOP_LINE},
+    {"bottom",  POS_BOTTOM_LINE},
+    {"fill",    POS_FILL}
+};
+
+static ctrl_code_t modes[] = {
+    {"rotate", MODE_ROTATE},
+    {"hold", MODE_HOLD},
+    {"flash", MODE_FLASH},
+    {"roll_up", MODE_ROLL_UP},
+    {"roll_down", MODE_ROLL_DOWN},
+    {"roll_left", MODE_ROLL_LEFT},
+    {"roll_right", MODE_ROLL_RIGHT},
+    {"wipe_up", MODE_WIPE_UP},
+    {"wipe_down", MODE_WIPE_DOWN},
+    {"wipe_left", MODE_WIPE_LEFT},
+    {"wipe_right", MODE_WIPE_RIGHT},
+    {"scroll", MODE_SCROLL},
+    {"automode", MODE_AUTOMODE},
+    {"roll_in", MODE_ROLL_IN},
+    {"roll_out", MODE_ROLL_OUT},
+    {"wipe_in", MODE_WIPE_IN},
+    {"wipe_out", MODE_WIPE_OUT},
+    {"compressed_rotate", MODE_COMPRESSED_ROTATE},
+    {"special", MODE_SPECIAL}
+};
+
+static ctrl_code_t special_modes[] = {
+    {"none", 0x00},
+    {"twinkle", SPECIAL_TWINKLE},
+    {"sparkle", SPECIAL_SPARKLE},
+    {"snow", SPECIAL_SNOW},
+    {"interlock", SPECIAL_INTERLOCK},
+    {"switch", SPECIAL_SWITCH},
+    {"slide", SPECIAL_SLIDE},
+    {"spray", SPECIAL_SPRAY},
+    {"starburst", SPECIAL_STARBURST},
+    {"script_welcome", SPECIAL_SCRIPT_WELCOME},
+    {"slot_machine", SPECIAL_SLOT_MACHINE}
+};
+
+// command line options, set to defaults
+static int port = 0;
+static int baud = 9600;
+static int msg_speed = 0;
+static int msg_color = 0;
+static int msg_pos = 0;
+static int msg_mode = 1;
+static int msg_special_mode = 0;
+
+void print_usage(char *cmd_name)
+{
+    unsigned int i;
+
+    fprintf(stdout, "%s [OPTION...]\n", cmd_name);
+    fprintf(stdout, "    -h, --help\n");
+    fprintf(stdout, "    --port=PORT (0 - 29)\n");
+    fprintf(stdout, "    --baud=BAUD\n");
+    fprintf(stdout, "        1200, 2400, 4800, 9600\n");
+    fprintf(stdout, "    -p, --position\n");
+    for (i = 0; i < NUM_POSITIONS; i++) {
+        fprintf(stdout, "        %d = %s\n", i, positions[i].name);
+    }
+    fprintf(stdout, "    -s, --speed=SPEED\n");
+    for (i = 0; i < NUM_SPEEDS; i++) {
+        fprintf(stdout, "        %d = %s\n", i, speeds[i].name);
+    }
+    fprintf(stdout, "    -c, --color=COLOR\n");
+    for (i = 0; i < NUM_COLORS; i++) {
+        fprintf(stdout, "        %d = %s\n", i, colors[i].name);
+    }
+    fprintf(stdout, "    -m, --mode=MODE\n");
+    for (i = 0; i < NUM_MODES; i++ ) {
+        fprintf(stdout, "        %d = %s\n", i, modes[i].name);
+    }
+    fprintf(stdout, "    --special=SPECIAL\n");
+    fprintf(stdout, "    value other than none must be specified if mode is set to special\n");
+    for (i = 0; i < NUM_SPECIAL_MODES; i++) {
+        fprintf(stdout, "        %d = %s\n", i, special_modes[i].name);
+    }
+};
+
+int parse_options(int argc, char **argv)
+{
+    int ret;
+    int option_index;
+    static struct option long_options[] = {
+        {"speed",       required_argument,  0, 's'},
+        {"port",        required_argument,  0, 0},
+        {"baud",        required_argument,  0, 0},
+        {"color",       required_argument,  0, 'c'},
+        {"help",        no_argument,        0, 'h'},
+        {"position",    required_argument,  0, 'p'},
+        {"mode",        required_argument,  0, 'm'},
+        {"special",     required_argument,  0, 0},
+        {0, 0, 0, 0}
+    };
+
+    while (1) {
+        if ((ret = getopt_long(argc, argv, "hs:c:p:m:", long_options, &option_index)) == -1)
+            break;
+
+        switch (ret) {
+            case 0:
+                if (strcmp(long_options[option_index].name,"port") == 0) {
+                    port = atoi(optarg);
+                    if (port < 0 || port >= NUM_PORTS) {
+                        fprintf(stderr, "error: invalid port\n");
+                        return -1;
+                    }
+                } else if (strcmp(long_options[option_index].name,"baud") == 0) {
+                    baud = atoi(optarg);
+                    if (baud != 1200 &&
+                            baud != 2400 &&
+                            baud != 4800 &&
+                            baud != 9600) {
+                        fprintf(stderr, "error: invalid baudrate\n");
+                        return -1;
+                    }
+                } else if (strcmp(long_options[option_index].name,"special") == 0) {
+                    msg_special_mode = atoi(optarg);
+                    if (msg_special_mode < 0 || msg_special_mode >= NUM_SPECIAL_MODES) {
+                        fprintf(stderr, "error: invalid special mode\n");
+                        return -1;
+                    }
+                    break;
+                } else {
+                    exit(-1);
+                }
+                break;
+            case 'p':
+                msg_pos = atoi(optarg);
+                if (msg_pos < 0 || msg_pos >= NUM_POSITIONS) {
+                    fprintf(stderr, "error: invalid message position\n");
+                    return -1;
+                }
+                break;
+            case 's':
+                msg_speed = atoi(optarg);
+                if (msg_speed < 0 || msg_speed >= NUM_SPEEDS) {
+                    fprintf(stderr, "error: invalid message speed\n");
+                    return -1;
+                }
+                break;
+            case 'c':
+                msg_color = atoi(optarg);
+                if (msg_color < 0 || msg_color >= NUM_COLORS) {
+                    fprintf(stderr, "error: invalid color\n");
+                    return -1;
+                }
+                break;
+            case 'm':
+                msg_mode = atoi(optarg);
+                if (msg_mode < 0 || msg_mode >= NUM_MODES) {
+                    fprintf(stderr, "error: invalid mode\n");
+                    return -1;
+                }
+                break;
+            case 'h':
+                print_usage(argv[0]);
+                exit(0);
+                break;
+            case '?': // unrecognized option, getopts_long will already have printed an error
+                exit(-1);
+                break;
+            default:
+                break;
+        };
+    }
+
+    if (modes[msg_mode].value == MODE_SPECIAL &&
+            special_modes[msg_special_mode].value == 0x00) {
+        fprintf(stderr, "error: special must be a value other than none when mode is set to special\n");
+        return -1;
+    }
+
+    if (special_modes[msg_special_mode].value != 0x00 &&
+            modes[msg_mode].value != MODE_SPECIAL) {
+        fprintf(stderr, "error: special cannot be set to a value other than none when mode is not set to special\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+void init_alpha_tx_hdr(alpha_tx_hdr_t *hdr)
+{
+    hdr->start = SOH;
+    hdr->type_code = ALL_MSG_CENTERS;
+    hdr->address = BROADCAST_ADDR;
+}
+
+void init_alpha_msg_hdr(alpha_msg_hdr_t *hdr)
+{
+    hdr->start = STX;
+    hdr->cmd = CMD_WRITE_TEXT;
+    hdr->file_label = 0x41; // file 'A'
+    hdr->esc = ESC;
+    hdr->display_pos = positions[msg_pos].value;
+    hdr->mode_code = modes[msg_mode].value;
+}
+
+int write_msg_to_sign(char *buffer, size_t len)
+{
+    alpha_tx_hdr_t tx_hdr;
+    alpha_msg_hdr_t msg_hdr;
+    uint32_t msg_begin = 0x00000000U;
+
+    init_alpha_tx_hdr(&tx_hdr);
+    init_alpha_msg_hdr(&msg_hdr);
+
+    // send null bytes to indicate new message
+    RS232_SendBuf(port, (unsigned char *)&msg_begin, sizeof(msg_begin));
+    RS232_SendBuf(port, (unsigned char *)&msg_begin, sizeof(msg_begin));
+
+    if (modes[msg_mode].value == MODE_SPECIAL) {
+        RS232_SendByte(port, special_modes[msg_special_mode].value);
+    }
+
+    RS232_SendByte(port, speeds[msg_speed].value);
+    RS232_SendByte(port, CTRL_COLOR);
+    RS232_SendByte(port, colors[msg_color].value);
+
+    // send transmission and message headers
+    RS232_SendBuf(port, (unsigned char *)&tx_hdr, sizeof(tx_hdr));
+    RS232_SendBuf(port, (unsigned char *)&msg_hdr, sizeof(msg_hdr));
+
+    // send text
+    RS232_SendBuf(port, (unsigned char *)buffer, len);
+
+    // send end of trasmission byte
+    RS232_SendByte(port,EOT);
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    char buffer[BUFFER_SIZE];
+
+    if (parse_options(argc, argv)) {
+        return -1;
+    }
+
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL && !feof(stdin)) {
+        fprintf(stderr, "error: unable to get input : %s\n", strerror(errno));
+        return -1;
+    }
+
+    fprintf(stderr, "got message: %s\n", buffer);
+
+    RS232_OpenComport(port, baud);
+
+    write_msg_to_sign(buffer, strlen(buffer));
+
+    RS232_CloseComport(port);
+
+    return 0;
+}
+
+#undef BUFFER_SIZE
+#undef NUM_PORTS
+#undef NUM_SPEEDS
+#undef NUM_COLORS
+#undef NUM_POSITIONS
+#undef NUM_MODES
+#undef NUM_SPECIAL_MODES
---
+++ sign_echo/sign_echo.h
@@ -0,0 +1,118 @@
+#ifndef __SIGN_ECHO__
+#define __SIGN_ECHO__
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <getopt.h>
+#include <errno.h>
+#include "rs232.h"
+
+// alpha protocol message control codes
+#define SOH 0x01 // start of header
+#define STX 0x02 // start of text
+#define EOT 0x04 // end of transmission
+#define ESC 0x1b // escape
+
+// unit type codes
+#define ALL_MSG_CENTERS 0x5a
+
+// special sign addresses
+#define BROADCAST_ADDR 0x3030
+
+// command codes
+#define CMD_WRITE_TEXT  0x41
+#define CMD_READ_TEXT   0x42
+
+// position codes
+#define POS_MIDDLE_LINE 0x20
+#define POS_TOP_LINE    0x22
+#define POS_BOTTOM_LINE 0x26
+#define POS_FILL        0x30
+
+// mode code
+#define MODE_ROTATE             0x61
+#define MODE_HOLD               0x62
+#define MODE_FLASH              0x63
+#define MODE_ROLL_UP            0x65
+#define MODE_ROLL_DOWN          0x66
+#define MODE_ROLL_LEFT          0x67
+#define MODE_ROLL_RIGHT         0x68
+#define MODE_WIPE_UP            0x69
+#define MODE_WIPE_DOWN          0x6a
+#define MODE_WIPE_LEFT          0x6b
+#define MODE_WIPE_RIGHT         0x6c
+#define MODE_SCROLL             0x6d
+#define MODE_AUTOMODE           0x6f
+#define MODE_ROLL_IN            0x70
+#define MODE_ROLL_OUT           0x71
+#define MODE_WIPE_IN            0x72
+#define MODE_WIPE_OUT           0x73
+#define MODE_COMPRESSED_ROTATE  0x74
+#define MODE_SPECIAL            0x6e
+
+// special specifiers
+#define SPECIAL_TWINKLE         0x30
+#define SPECIAL_SPARKLE         0x31
+#define SPECIAL_SNOW            0x32
+#define SPECIAL_INTERLOCK       0x33
+#define SPECIAL_SWITCH          0x34
+#define SPECIAL_SLIDE           0x35
+#define SPECIAL_SPRAY           0x36
+#define SPECIAL_STARBURST       0x37
+#define SPECIAL_SCRIPT_WELCOME  0x38
+#define SPECIAL_SLOT_MACHINE    0x39
+
+// control codes
+
+// speed
+#define NO_HOLD 0x09
+#define SPEED_1 0x15
+#define SPEED_2 0x16
+#define SPEED_3 0x17
+#define SPEED_4 0x18
+#define SPEED_5 0x19
+
+// character set
+#define CTRL_CHAR_SET   0x1a
+#define CHAR_SET_1      0x31 // five high standard
+#define CHAR_SET_3      0x33 // seven high standard
+#define CHAR_SET_5      0x35 // seven high fancy
+#define CHAR_SET_6      0x36 // ten high standard
+#define CHAR_SET_8      0x38 // full height fancy
+#define CHAR_SET_9      0x39 // full height standard
+
+// colors
+#define CTRL_COLOR      0x1c
+#define COLOR_RED       0x31
+#define COLOR_GREEN     0x32
+#define COLOR_AMBER     0x33
+#define COLOR_DIM_RED   0x34
+#define COLOR_DIM_GREEN 0x35
+#define COLOR_BROWN     0x36
+#define COLOR_ORANGE    0x37
+#define COLOR_YELLOW    0x38
+#define COLOR_RAINBOW1  0x39
+#define COLOR_RAINBOW2  0x41
+#define COLOR_COLOR_MIX 0x42
+#define COLOR_AUTOCOLOR 0x43
+
+typedef struct {
+    char    name[128];
+    uint8_t value;
+} ctrl_code_t;
+
+typedef struct {
+    uint8_t start;
+    uint8_t type_code;
+    uint16_t address;
+} alpha_tx_hdr_t;
+
+typedef struct {
+    uint8_t start;
+    uint8_t cmd;
+    uint8_t file_label;
+    uint8_t esc;
+    uint8_t display_pos;
+    uint8_t mode_code;
+} alpha_msg_hdr_t;
+#endif
